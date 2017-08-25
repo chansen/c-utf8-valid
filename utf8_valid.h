9ad1fdd84e8205e2f09053d821c276c7fed0efd7
chansen <chansen@cpan.org>
2017-08-25 15:13:26 +0200

    Added comments

--- utf8_valid.h
+++ utf8_valid.h
@@ -34,6 +34,34 @@
 extern "C" {
 #endif
 
+/*
+ *    UTF-8 Encoding Form
+ *
+ *    U+0000..U+007F       0xxxxxxx
+ *    U+0080..U+07FF       110xxxxx 10xxxxxx
+ *    U+0800..U+FFFF       1110xxxx 10xxxxxx 10xxxxxx
+ *   U+10000..U+10FFFF     11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+ *
+ *
+ *    U+0000..U+007F       00..7F
+ *                      N  C0..C1  80..BF                   1100000x 10xxxxxx
+ *    U+0080..U+07FF       C2..DF  80..BF
+ *                      N  E0      80..9F  80..BF           11100000 100xxxxx
+ *    U+0800..U+0FFF       E0      A0..BF  80..BF
+ *    U+1000..U+CFFF       E1..EC  80..BF  80..BF
+ *    U+D000..U+D7FF       ED      80..9F  80..BF
+ *                      S  ED      A0..BF  80..BF           11101101 101xxxxx
+ *    U+E000..U+FFFF       EE..EF  80..BF  80..BF
+ *                      N  F0      80..8F  80..BF  80..BF   11110000 1000xxxx
+ *   U+10000..U+3FFFF      F0      90..BF  80..BF  80..BF
+ *   U+40000..U+FFFFF      F1..F3  80..BF  80..BF  80..BF
+ *  U+100000..U+10FFFF     F4      80..8F  80..BF  80..BF   11110100 1000xxxx
+ *
+ *  Legend:
+ *    N = Non-shortest form
+ *    S = Surrogates
+ */
+
 bool
 utf8_check(const char *src, size_t len, size_t *cursor) {
   const unsigned char *cur = (const unsigned char *)src;
@@ -52,13 +80,16 @@ utf8_check(const char *src, size_t len, size_t *cursor) {
     }
 
     v = p[0];
+    /* 0xxxxxxx */
     if ((v & 0x80) == 0) {
       cur += 1;
       continue;
     }
 
     v = (v << 8) | p[1];
+    /* 110xxxxx 10xxxxxx */
     if ((v & 0xE0C0) == 0xC080) {
+      /* Ensure that the top 4 bits is not zero */
       v = v & 0x1E00;
       if (v == 0)
         break;
@@ -67,7 +98,9 @@ utf8_check(const char *src, size_t len, size_t *cursor) {
     }
 
     v = (v << 8) | p[2];
+    /* 1110xxxx 10xxxxxx 10xxxxxx */
     if ((v & 0xF0C0C0) == 0xE08080) {
+      /* Ensure that the top 5 bits is not zero and not a surrogate */
       v = v & 0x0F2000;
       if (v == 0 || v == 0x0D2000)
         break;
@@ -76,7 +109,9 @@ utf8_check(const char *src, size_t len, size_t *cursor) {
     }
 
     v = (v << 8) | p[3];
+    /* 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx */
     if ((v & 0xF8C0C0C0) == 0xF0808080) {
+      /* Ensure that the top 5 bits is not zero and not out of range */
       v = v & 0x07300000;
       if (v == 0 || v > 0x04000000)
         break;
